fee90e021363dfbc81f581f8be4c5b1c474e2bb2
Jannis Maier <jannismaier97@gmail.com>
2023-03-17 14:38:14 +0100

    Adding submodules, fixing build

--- UITest/src/main.cpp
+++ UITest/src/main.cpp
@@ -353,7 +353,7 @@ static void FramePresent(ImGui_ImplVulkanH_Window* wd)
 }
 
 // Main code
-int main(int, char**)
+int main(int argc, char** argv)
 {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
